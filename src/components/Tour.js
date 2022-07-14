aeaf018d46f7939a5f0937e85598c7a66487eb6b
blitzmartin <lizmartinmusic@gmail.com>
2022-07-14 10:54:04 +0200

    small changes

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -4,7 +4,7 @@ export default function Tour({ id, name, info, image, price }) {
   return (
     <div className="single-tour">
       <h3>{name}</h3>
-      <h5>{price}</h5>
+      <h5>${price}</h5>
       <p>{info}</p>
       <img src={image} alt={name} />
     </div>
