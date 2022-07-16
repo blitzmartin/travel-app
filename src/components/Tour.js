5eae85f99c8260164ba8ca4361d72c2fe006c93c
blitzmartin <lizmartinmusic@gmail.com>
2022-07-16 15:11:40 +0200

    fixed img size in card

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -5,12 +5,12 @@ import { Card } from "react-bootstrap";
 export default function Tour({ id, name, info, image, price }) {
   return (
     <Card className="card-style">
-       <Card.Img variant="top" src={image} alt={name} />
+       <Card.Img className="card-img" variant="top" src={image} alt={name} />
        <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">${price}</Card.Subtitle>
         <Card.Text>{info}</Card.Text>
-        <Button variant="primary">Not Interested</Button>
+        <Button variant="danger">Not Interested</Button>
       </Card.Body>
     </Card>
   );
