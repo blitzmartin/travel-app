9e89f36a78697a069085741176d8c5d597b21a30
blitzmartin <lizmartinmusic@gmail.com>
2022-07-16 16:52:15 +0200

    read more btn

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -4,13 +4,16 @@ import { Card } from "react-bootstrap";
 
 export default function Tour({ id, name, info, image, price }) {
   const [readMore, setReadMore] = useState(false);
+  const handleClick = () => {
+    setReadMore(true);
+  }
   return (
     <Card className="card-style">
       <Card.Img className="card-img" variant="top" src={image} alt={name} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">${price}</Card.Subtitle>
-        <Card.Text>{readMore ? info :info.substring(0, 200)}</Card.Text>
+        <Card.Text>{readMore ? info : `${info.substring(0, 230)}...`}<Button variant="link" size="sm" onClick={handleClick} className="link-btn">Read more</Button></Card.Text>
         <Button variant="danger">Not Interested</Button>
       </Card.Body>
     </Card>
