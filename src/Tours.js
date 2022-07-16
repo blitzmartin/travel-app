b64b8f5b4bcb0816c4848a68644a391828512793
blitzmartin <lizmartinmusic@gmail.com>
2022-07-16 14:49:26 +0200

    added bootstrap card to tour

--- src/Tours.js
+++ src/Tours.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Tour from "./components/Tour.js";
+import Container from "react-bootstrap/esm/Container.js";
 
 export default function Tours({ tours }) {
   return (
-    <div>
+    <Container>
       <h1>Tour</h1>
       {tours.map((tour) => {
         return <Tour key={tour.id} {...tour} />;
       })}
-    </div>
+    </Container>
   );
 }
--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default function Tour({ id, name, info, image, price }) {
   return (
-    <div className="single-tour">
-      <h3>{name}</h3>
-      <h5>${price}</h5>
-      <p>{info}</p>
-      <img src={image} alt={name} />
-    </div>
+    <Card className="card-style">
+       <Card.Img variant="top" src={image} alt={name} />
+       <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">${price}</Card.Subtitle>
+        <Card.Text>{info}</Card.Text>
+        <Button variant="primary">Not Interested</Button>
+      </Card.Body>
+    </Card>
   );
 }
